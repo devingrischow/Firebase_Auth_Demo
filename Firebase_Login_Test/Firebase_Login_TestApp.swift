e2e8864dd3a64a57e6e1a114d442e406cb410611
devingrischow <devingrischow@outlook.com>
2023-11-09 18:13:15 -0600

    made me

--- Firebase_Login_Test/Firebase_Login_TestApp.swift
+++ Firebase_Login_Test/Firebase_Login_TestApp.swift
@@ -13,6 +13,8 @@ struct Firebase_Login_TestApp: App {
     // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
